0393a3027dce586abbad9538b65e4ac349156b07
이건희 <qwertys1347@gmail.com>
2025-11-07 15:23:55 +0900

    refactor: 챗봇 레이아웃 리팩토링

--- app/chatbot/components/chatbot.py
+++ app/chatbot/components/chatbot.py
@@ -1,4 +1,7 @@
+import time
 import streamlit as st
+
+from streamlit.delta_generator import DeltaGenerator
 
 from config import ASSETS_PATH, STORAGE_PATH
 from common.utils.image import get_base64_image
@@ -29,3 +32,66 @@ def markdown_assistant_style():
         }}
         </style>
     """, unsafe_allow_html=True)
+
+
+def init_chatbot_session():
+    if "messages" not in st.session_state:
+        st.session_state.messages = [
+            {"role": "assistant", "content": "안녕하세요! 🖐️ Agent 챗봇이에요<br/><br/>궁금하신 점이 있으면 질문을 입력해 주세요 😊"}
+        ]
+
+
+def render_chatbot_container(messages):
+    st.markdown('<div class="chat-container">', unsafe_allow_html=True)
+
+    # 기존 메시지 렌더링
+    for msg in messages:
+        if msg["role"] == "user":
+            st.markdown(f"""
+                <div class="chat-row user-row">
+                    <div class="chat-bubble user-bubble">{msg["content"]}</div>
+                </div>
+            """, unsafe_allow_html=True)
+
+        else:
+            st.markdown(f"""
+                <div class="chat-row">
+                    <div class="assistant-profile"></div>
+                    <div class="chat-bubble assistant-bubble">{msg["content"]}</div>
+                </div>
+            """, unsafe_allow_html=True)
+
+    st.markdown('</div>', unsafe_allow_html=True)
+
+
+def render_user_input(prompt: str):
+    st.markdown(f"""
+        <div class="chat-row user-row">
+            <div class="chat-bubble user-bubble">{prompt}</div>
+        </div>
+    """, unsafe_allow_html=True)
+
+
+def render_assist_typing_placeholder(typing_placeholder: DeltaGenerator):
+    typing_placeholder.markdown(f"""
+        <div class="chat-row" style="align-items: center; gap: 8px; margin-top: 6px;">
+            <div class="assistant-profile"></div>
+            <div class="typing">
+                <span></span><span></span><span></span>
+            </div>
+        </div>
+    """, unsafe_allow_html=True)
+
+
+def render_assist_output(message_placeholder: DeltaGenerator, output: str):
+    typed_text = ""
+
+    for char in output:
+        typed_text += char
+        message_placeholder.markdown(f"""
+            <div class="chat-row">
+                <div class="assistant-profile"></div>
+                <div class="chat-bubble assistant-bubble">{typed_text}</div>
+            </div>
+        """, unsafe_allow_html=True)
+        time.sleep(0.03)
--- pages/chatbot.py
+++ pages/chatbot.py
@@ -1,7 +1,8 @@
 import time
 import streamlit as st
 
-from app.chatbot.components.chatbot import markdown_assistant_style, markdown_chatbot_style
+from app.chatbot.components.chatbot import init_chatbot_session, markdown_assistant_style, markdown_chatbot_style, render_assist_output, render_assist_typing_placeholder, render_chatbot_container, render_user_input
+from app.chatbot.components.sidebar import render_sidebar
 from app.chatbot.services.chatbot_service import ChatbotService
 
 
@@ -9,80 +10,32 @@ st.set_page_config(page_title="chatbot", page_icon="💬", layout="centered")
 
 markdown_chatbot_style()
 markdown_assistant_style()
-
+render_sidebar()
 
 chatbot_service = ChatbotService()
 
-
-
 # 세션 초기화
-if "messages" not in st.session_state:
-    st.session_state.messages = [
-        {"role": "assistant", "content": "안녕하세요! 🖐️ Agent 챗봇이에요<br/><br/>궁금하신 점이 있으면 질문을 입력해 주세요 😊"}
-    ]
+init_chatbot_session()
 
-st.sidebar.button("🧹 새 대화", on_click=lambda: st.session_state.clear())
-
-# 채팅 컨테이너 시작
-st.markdown('<div class="chat-container">', unsafe_allow_html=True)
-
-# 기존 메시지 렌더링
-for msg in st.session_state.messages:
-    if msg["role"] == "user":
-        st.markdown(f"""
-            <div class="chat-row user-row">
-                <div class="chat-bubble user-bubble">{msg["content"]}</div>
-            </div>
-        """, unsafe_allow_html=True)
-
-    else:
-        st.markdown(f"""
-            <div class="chat-row">
-                <div class="assistant-profile"></div>
-                <div class="chat-bubble assistant-bubble">{msg["content"]}</div>
-            </div>
-        """, unsafe_allow_html=True)
-
-st.markdown('</div>', unsafe_allow_html=True)
+# 채팅 컨테이너 표시
+render_chatbot_container(st.session_state.messages)
 
 # 입력창
 if prompt := st.chat_input("메시지를 입력하세요..."):
     # 사용자 메시지 저장 및 표시
     st.session_state.messages.append({"role": "user", "content": prompt})
-    st.markdown(f"""
-        <div class="chat-row user-row">
-            <div class="chat-bubble user-bubble">{prompt}</div>
-        </div>
-    """, unsafe_allow_html=True)
+    render_user_input(prompt)
 
-    # GPT 입력중 표시
+    # AI 입력중 표시
     typing_placeholder = st.empty()
-    typing_placeholder.markdown(f"""
-        <div class="chat-row" style="align-items: center; gap: 8px; margin-top: 6px;">
-            <div class="assistant-profile"></div>
-            <div class="typing">
-                <span></span><span></span><span></span>
-            </div>
-        </div>
-    """, unsafe_allow_html=True)
+    render_assist_typing_placeholder(typing_placeholder)
     time.sleep(1.0)
 
-    # 답변 생성
-    reply = chatbot_service.get_chatbot_output(prompt)
-
+    # AI 답변 생성
+    output = chatbot_service.get_chatbot_output(prompt)
     typing_placeholder.empty()
 
-    # 타이핑 애니메이션
+    # AI 메세지 타이핑 애니메이션 및 표시
     message_placeholder = st.empty()
-    typed_text = ""
-    for char in reply:
-        typed_text += char
-        message_placeholder.markdown(f"""
-            <div class="chat-row">
-                <div class="assistant-profile"></div>
-                <div class="chat-bubble assistant-bubble">{typed_text}</div>
-            </div>
-        """, unsafe_allow_html=True)
-        time.sleep(0.03)
-
-    st.session_state.messages.append({"role": "assistant", "content": reply})
+    render_assist_output(message_placeholder, output)
+    st.session_state.messages.append({"role": "assistant", "content": output})
